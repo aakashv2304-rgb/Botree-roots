fd47fe3780dcedb279de56c022f12635654a877d
aakashv2304-rgb <aakashv2304@gmail.com>
2026-07-31 12:48:37 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from starlette.middleware.cors import CORSMiddleware
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
+import certifi
 import logging
 from pathlib import Path
 from pydantic import BaseModel, EmailStr, Field
@@ -42,7 +43,7 @@ load_dotenv(ROOT_DIR / '.env')
 
 # MongoDB connection
 mongo_url = os.environ['MONGO_URL']
-client = AsyncIOMotorClient(mongo_url)
+client = AsyncIOMotorClient(mongo_url, tlsCAFile=certifi.where())
 db = client[os.environ['DB_NAME']]
 
 # Create the main app
