0301c041a217fa06e3bd104cf29f3b01917a8b0a
aakashv2304-rgb <aakashv2304@gmail.com>
2026-08-10 14:03:17 +0530

    Update ProtectedRoute.js

--- frontend/src/components/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute.js
@@ -8,7 +8,7 @@ const ProtectedRoute = ({ children }) => {
   if (loading) {
     return (
       <div className="flex items-center justify-center min-h-screen">
-        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-[#0F172A]"></div>
+        <div className="animate-spin rounded-full h-10 w-12 border-t-2 border-b-2 border-[#0F172A]"></div>
       </div>
     );
   }
